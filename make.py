c53c03c82d9ee319dc8f37ed4375910e44aa7633
Jeanelle Ackerman <jeanelle.ack@gmail.com>
2019-04-05 15:04:22 -0400

    added new dataset BBBC043 (#68)

--- make.py
+++ make.py
@@ -59,6 +59,7 @@ IMAGE_SETS = {'BBBC001': u'Human HT29 colon-cancer cells',
               #BBBC040 is reserved but not ready to be online yet
               'BBBC041': u'P. vivax (malaria) infected human blood smears',
               'BBBC042': u'Rat astrocyte cells',
+              'BBBC043': u'Mouse hippocampal presynaptic terminals (vGlut-1 and vGat)',
 }
 
 PAGES = [('index', 'Introduction', ''),
