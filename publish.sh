0505d1e28a48331bf925eb93b36253d6286ee95c
Shantanu Singh <shsingh@broadinstitute.org>
2022-02-11 16:52:41 -0500

    Delete publish.sh

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-base=`dirname "$0"`
-broad_server='login01'
-umask 002
-rsync -crl --exclude=.git --exclude=.gitignore --exclude=.DS_Store --exclude=foo --verbose --delete --chmod=ugo=rwX $base/htdocs/ ${broad_server}:/imaging/web/BBBC/htdocs/
